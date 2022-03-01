80b0cc7d20e92decee12835d01eb79b003b97e02
Deividy <deividyosantos27@gmail.com>
2022-02-28 23:45:46 -0400

    adiciona fundo a show pokemon

--- resources/views/myPokemons.blade.php
+++ resources/views/myPokemons.blade.php
@@ -11,7 +11,7 @@
     @if(Auth::user()->trainerPokemon->count())
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg pb-8 border-b border-gray-200">
+                <div class="bg-white shadow-sm sm:rounded-lg pb-8">
                     <div class="p-6 pb-24 bg-white grid grid-cols-4">
 
                         @foreach(Auth::user()->trainerPokemon as $trainerPokemon)
--- resources/views/showPokemon.blade.php
+++ resources/views/showPokemon.blade.php
@@ -1,52 +1,55 @@
 <x-app-layout>
-    <div class="text-center mx-auto  w-ful h-full">
-        <div class="mt-20">
-            <div class="py-2">
-                <x-auth-validation-errors class="mb-4" :errors="$errors" />
-                <form class="flex flex-row justify-center" action="{{route('nicknamePokemon')}}" method="POST">
-                    @csrf
-                    <div>
-                        <x-label for="nickname">Nickname</x-label>
-                        @if($trainerPokemon->nickName != '')
-                            <x-input id="nickname" name="nicknamePokemon" type="text" value="{{$trainerPokemon->nickName}}"/>
-                        @else
-                            <x-input id="nickname" name="nicknamePokemon" type="text" value="{{ucfirst($payload['name'])}}"/>
-                        @endif
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="rounded p-8 shadow-sm sm:rounded-lg bg-white">
+                <div class="py-2">
+                    <x-auth-validation-errors class="mb-4 max-w-max mx-auto" :errors="$errors" />
+                    <form class="flex flex-row justify-center" action="{{route('nicknamePokemon')}}" method="POST">
+                        @csrf
+                        <div>
+                            <x-label for="nickname">Nickname</x-label>
+                            @if($trainerPokemon->nickName != '')
+                                <x-input id="nickname" name="nicknamePokemon" type="text" value="{{$trainerPokemon->nickName}}"/>
+                            @else
+                                <x-input id="nickname" name="nicknamePokemon" type="text" value="{{ucfirst($payload['name'])}}"/>
+                            @endif
 
-                        <input type="hidden" name="trainerPokemonId" value="{{$trainerPokemon->id}}">
-                    </div>
-                    <div class="ml-2 mt-6">
-                        <button class="w-8 h-8" onclick="submit()"><x-iconpark-saveone-o/></button>
-                    </div>
-                </form>
-            </div>
-            <img class="mx-auto w-40 h-40" src="{{$payload['image_url']}}" alt="{{$payload['name']}}">
-        </div>
-        <div class="flex flex-row justify-evenly text-left">
-            <div class="">
-                <h3 >Types: </h3>
-                @foreach($payload['types'] as $type)
-                    <p class="capitalize ml-3">{{$type}}</p>
-                @endforeach
-            </div>
-            <div>
-                <p>Height: {{$payload['height']}}</p>
-                <p>Weight: {{$payload['weight']}}</p>
-            </div>
-            <div>
-                <h3>Abilities: </h3>
-                <div>
-                    @foreach($payload['abilities'] as $ability)
-                        <p class="capitalize ml-3">{{$ability}}</p>
-                    @endforeach
+                            <input type="hidden" name="trainerPokemonId" value="{{$trainerPokemon->id}}">
+                        </div>
+                        <div class="ml-2 mt-6">
+                            <button class="w-8 h-8" onclick="submit()"><x-iconpark-saveone-o/></button>
+                        </div>
+                    </form>
                 </div>
-            </div>
-            <div>
-                <h3>Stats:</h3>
-                <div>
-                    @foreach($payload['stats'] as $stat => $value)
-                        <p class="capitalize ml-3">{{$stat}}: {{$value}}</p>
-                    @endforeach
+
+                <img class="mx-auto w-40 h-40" src="{{$payload['image_url']}}" alt="{{$payload['name']}}">
+                <div class="flex flex-row justify-evenly text-left">
+                    <div class="">
+                        <h3>Types: </h3>
+                        @foreach($payload['types'] as $type)
+                            <p class="capitalize">{{$type}}</p>
+                        @endforeach
+                    </div>
+                    <div>
+                        <p>Height: {{$payload['height']}}</p>
+                        <p>Weight: {{$payload['weight']}}</p>
+                    </div>
+                    <div>
+                        <h3>Abilities: </h3>
+                        <div>
+                            @foreach($payload['abilities'] as $ability)
+                                <p class="capitalize ml-3">{{$ability}}</p>
+                            @endforeach
+                        </div>
+                    </div>
+                    <div>
+                        <h3>Stats:</h3>
+                        <div>
+                            @foreach($payload['stats'] as $stat => $value)
+                                <p class="capitalize ml-3">{{$stat}}: {{$value}}</p>
+                            @endforeach
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
