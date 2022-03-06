6c45988b3537e2351778a49cb847baa96d93eeb1
Deividy <deividyosantos27@gmail.com>
2022-03-06 16:02:45 -0400

    feat: add animation

--- resources/views/capturePokemon.blade.php
+++ resources/views/capturePokemon.blade.php
@@ -16,17 +16,16 @@
         </form>
     </div>
 
-    <div class="max-w-max mx-auto bg-white rounded grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6">
+    <div class="max-w-max mx-auto bg-white rounded grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-6 shadow-lg">
         @foreach($pokemons as $pokemon)
-            <div class="flex flex-col flex-wrap capitalize items-center ml-4 mt-4">
-                <button onclick="copyPokemonName('{{$pokemon->name}}')">
+            <div class="flex flex-col bg-gray-50 flex-wrap items-center m-4 shadow-lg shadow-indigo-200/40">
+                <button class="capitalize" onclick="copyPokemonName('{{$pokemon->name}}')">
                     <img class="w-32 h-32" src="{{$pokemon->image_url}}" alt="{{$pokemon->name}}">
-                </button>
 
-
-                <div class="flex flex-row flex-wrap justify-center">
+                    <div class="flex flex-row flex-wrap justify-center">
                         <p>{{$pokemon->name}}</p>
-                </div>
+                    </div>
+                </button>
             </div>
         @endforeach
     </div>
@@ -34,7 +33,8 @@
     <div class="flex flex-row mx-auto w-8 mt-4 pb-8">
         <div>
             <a
-                class="px-4 mr-4 {{$paginate == 1 ? 'bg-gray-200' : 'bg-white'}}" href="{{ $paginate != 1 ? route('capturePokemonView', $paginate - 1) : ''}}">
+                class="px-4 mr-4 {{$paginate == 1 ? 'bg-gray-200' : 'bg-white'}}"
+                href="{{ $paginate != 1 ? route('capturePokemonView', $paginate - 1) : ''}}">
                 <
             </a>
         </div>
@@ -53,6 +53,17 @@
                 let input = document.getElementById('inputName');
 
                 input.value = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
+
+                windowToTop();
+            }
+
+            function windowToTop()
+            {
+                window.scrollTo({
+                    top: -100,
+                    left: 0,
+                    behavior: 'smooth'
+                });
             }
         </script>
     </x-slot>
